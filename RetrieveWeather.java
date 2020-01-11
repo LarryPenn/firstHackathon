3af853cde2881dcfcb98e763d4aca8e2ee58cd0a
Joshua Chopra <jchopra@seas.upenn.edu>
2020-01-10 23:40:40 -0500

    Put all of code into methods (removed from main method) to properly return hashmap with needed weather attributes

--- RetrieveWeather.java
+++ RetrieveWeather.java
@@ -1,20 +1,32 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class RetrieveWeather {
 
-//    String weatherAPIURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
-    String APIKey = "8365433bc5b8ac876c7f82b0af27a5e5"; // TODO change this to user input from console
+    String weatherDescription;
+    String currentTemp;
+    String minTemp;
+    String maxTemp;
+    String tempFeelsLike;
+//    my key 8365433bc5b8ac876c7f82b0af27a5e5
+    String APIKey = "49b96c058384bf0defca868c4cfc1932"; // TODO change this to user input from console
     static String weatherAPIURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
 
+    /***
+     * Helper method used to create URL to pass to API with given API key, and zipcode.
+     * @param apiKey - users API key
+     * @param zipCode - users zip code
+     * @return - URL in properly encoded format to query API.
+     */
     private static URL createURL(String apiKey, String zipCode) {
         URL url = null;
         try {
@@ -25,6 +37,12 @@ public class RetrieveWeather {
         return url;
     }
 
+    /***
+     * Helper method used from com.google.gson.Gson library that parses JSON objects and returns
+     * them as HashMap
+     * @param str JSON object as string
+     * @return JSON object to hashmap
+     */
     private static HashMap<String, Object> jsonToHashMap(String str) {
         HashMap<String, Object> weatherMap = new Gson().fromJson(str,
                 new TypeToken<HashMap<String, Object>>() {
@@ -33,60 +51,57 @@ public class RetrieveWeather {
         return weatherMap;
     }
 
+    /***
+     * This method takes in the apiKey to be used as well as the user's zipcode and queries the
+     * openweather.org website for weather data for that user's zipcode, using the apikey.
+     * @param apiKey
+     * @param zipCode
+     * @return
+     * @throws IOException
+     */
     public HashMap<String, String> getWeatherAttributes(String apiKey, String zipCode) throws IOException {
-//        URL url = new URL(createURL(apiKey, zipCode));
         URLConnection conn = createURL(apiKey, zipCode).openConnection();
         StringBuilder jsonData = new StringBuilder();
+        HashMap<String, String> weatherInfo = new HashMap<String, String>();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()))){
             String line;
             while ((line = reader.readLine()) != null) {
                 jsonData.append(line);
             }
             HashMap<String, Object> responseMap = jsonToHashMap(jsonData.toString());
+            ArrayList<Map> weather = (ArrayList) responseMap.get("weather");
+            weatherDescription = weather.get(0).get("description").toString();
+
+            Map<String, Double> temps = (Map<String, Double>) responseMap.get("main");
+            currentTemp = temps.get("temp").toString();
+            minTemp = temps.get("temp_min").toString();
+            maxTemp = temps.get("temp_max").toString();
+            tempFeelsLike = temps.get("feels_like").toString();
+
+            // add attributes to hashmap, which we'll output & format nicely for sending text message.
+            weatherInfo.put("weather description", weatherDescription);
+            weatherInfo.put("current temperature", currentTemp);
+            weatherInfo.put("low temperature", minTemp);
+            weatherInfo.put("maximum temperature", maxTemp);
+            weatherInfo.put("temp feels like", tempFeelsLike);
+
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        return String[]; //TODO
+        return weatherInfo;
     }
 
 
-
     public static void main(String[] args) {
-        String my_API_KEY = "8365433bc5b8ac876c7f82b0af27a5e5";
-//        System.out.println("Please Enter API Key");
-        String zipCode = "45040";
-        String urlString = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us" + "&appid=" +
-                my_API_KEY + "&units=imperial";
+        RetrieveWeather w = new RetrieveWeather();
         try {
-            StringBuilder jsonData = new StringBuilder();
-            URL url = new URL(urlString);
-            URLConnection conn = url.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                jsonData.append(line);
-            }
-            reader.close();
-            String separator = "==================================================================================" +
-                    "=======================";
-            System.out.println("JSON data " + '\n' + separator + '\n' + jsonData);
-            HashMap<String, Object> responseMap = jsonToHashMap(jsonData.toString());
-            HashMap<String, Object> mainMap = jsonToHashMap(responseMap.get("main").toString());
-//            HashMap<String, Object> weatherMap = jsonToHashMap(responseMap.get("weather").toString());
-            System.out.println('\n' + "JSON to Map" + '\n' +
-                    separator + '\n' + responseMap);
-            System.out.println('\n' + "Value from Main Key in responseMap" + '\n' +
-                    separator + '\n' +mainMap);
-            System.out.println('\n' + "Keys in map" + '\n' + responseMap.keySet().toString());
-            System.out.println('\n' + "Weather is: " + '\n' + separator + '\n' + responseMap.get("weather"));
-        } catch (IOException e) {
+            HashMap<String, String> pleaseWork = w.getWeatherAttributes("8365433bc5b8ac876c7f82b0af27a5e5",
+                    "45040");
+            System.out.println(pleaseWork);
+        }
+        catch (IOException e) {
             System.out.println(e.getMessage());
         }
-
-
-
-
-
     }
 }
