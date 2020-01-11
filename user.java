46ffc83a9154dabf5b4d593403f3249c509053d8
LarryPenn <49259555+LarryPenn@users.noreply.github.com>
2020-01-10 17:30:22 -0800

    Add files via upload

---
+++ user.java
@@ -0,0 +1,30 @@
+package weatherText;
+
+public class user {
+
+	private String name;
+	private String phoneNumber;
+	private String zipCode;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+	
+	
+}
---
+++ weather.java
@@ -0,0 +1,24 @@
+package weatherText;
+
+public class weather {
+
+	String main;
+	double temp;
+	
+	
+	public String getMain() {
+		return main;
+	}
+	public void setMain(String main) {
+		this.main = main;
+	}
+	public double getTemp() {
+		return temp;
+	}
+	public void setTemp(double temp) {
+		this.temp = temp;
+	}
+	
+	
+	
+}
