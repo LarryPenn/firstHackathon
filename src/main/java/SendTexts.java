bfd115d8ae9b1bb64fe84c83cc7fb1b4306fb6d7
Joshua Chopra <jchopra@seas.upenn.edu>
2020-01-11 12:48:20 -0500

    Re-added all files, maven dependencies, google JSON library, etc.

---
+++ src/main/java/SendTexts.java
@@ -0,0 +1,31 @@
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+
+/***
+ * Class is taken as is from twilio.com to send text messages. Simply just redesigned method to take in a phone
+ * number and message when creating a message instance.
+ */
+public class SendTexts {
+    // Install the Java helper library from twilio.com/docs/java/install
+
+    // Find your Account Sid and Token at twilio.com/console
+    // DANGER! This is insecure. See http://twil.io/secure
+    public static final String ACCOUNT_SID = "ACd2a83fbfbea92c00e25a54b9b8466f7f";
+//    public static final String AUTH_TOKEN = "Enter auth token";
+
+    public static void sendMessage(String phoneNumber, String weatherMessage, String AUTH_TOKEN) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+1" + phoneNumber),
+                new com.twilio.type.PhoneNumber("+1 201 903 9808"),
+                weatherMessage)
+                .create();
+        System.out.println("Success " + message.getSid());
+    }
+
+    public static void main(String[] args) {
+        SendTexts.sendMessage("5132828992", "hello Josh",
+                "353de2ebdb036ffd25e44621ee9e78df");
+    }
+
+}
